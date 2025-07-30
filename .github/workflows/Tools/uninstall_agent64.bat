33c8eecd7e03917006ba7bdd17b273abfd136aae
PeterVeg <ppcornilleau@gmail.com>
2025-07-30 14:48:03 +0200

    Add files via upload

---
+++ .github/workflows/Tools/uninstall_agent64.bat
@@ -0,0 +1,28 @@
+REM	//////////////////////////////////////////////////////////////////////////////////
+REM	// Symantec copyright header start
+REM	//////////////////////////////////////////////////////////////////////////////////
+REM	//
+REM
+REM Copyright (c) 2020 Broadcom. All Rights Reserved.
+REM The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+REM
+REM THIS SOFTWARE CONTAINS CONFIDENTIAL INFORMATION AND TRADE SECRETS OF
+REM BROADCOM. USE, DISCLOSURE OR REPRODUCTION IS PROHIBITED WITHOUT THE PRIOR
+REM EXPRESS WRITTEN PERMISSION OF BROADCOM.
+REM
+REM The Licensed Software and Documentation are deemed to be commercial computer
+REM software as defined in FAR 12.212 and subject to restricted rights as
+REM defined in FAR Section 52.227-19 "Commercial Computer Software - Restricted
+REM Rights" and DFARS 227.7202, Rights in "Commercial Computer Software or
+REM Commercial Computer Software Documentation," as applicable, and any successor
+REM regulations, whether delivered by Broadcom as on premises or hosted services.
+REM Any use, modification, reproduction release, performance, display or
+REM disclosure of the Licensed Software and Documentation by the U.S. Government
+REM shall be solely in accordance with the terms of this Agreement.
+REM
+
+REM	//
+REM	//////////////////////////////////////////////////////////////////////////////////
+REM	// Symantec copyright header stop
+REM	//////////////////////////////////////////////////////////////////////////////////
+msiexec /uninstall {05549D95-A5AF-4679-8125-6152D960684B} /q UNINSTALLPASSWORD=""
